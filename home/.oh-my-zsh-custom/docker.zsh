6a1ee6a0574a0caff59377b3456482356c7964ba
Bruno Maia <bruno.mm.maia@gmail.com>
2015-05-10 21:22:13 +0100

    Only export docker env vars on osx

--- home/.oh-my-zsh-custom/docker.zsh
+++ home/.oh-my-zsh-custom/docker.zsh
@@ -1,4 +1,6 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/bruno/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+if [[ "$OSTYPE" = darwin* ]]; then
+  export DOCKER_HOST=tcp://192.168.59.103:2376
+  export DOCKER_CERT_PATH=/Users/bruno/.boot2docker/certs/boot2docker-vm
+  export DOCKER_TLS_VERIFY=1
+fi
 
