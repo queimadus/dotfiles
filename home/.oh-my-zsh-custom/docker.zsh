23f7ed598dfe26b162de056caf163744eab4d1fb
Bruno Maia <bruno.mm.maia@gmail.com>
2015-08-30 13:10:07 +0100

    update docker env variables (boot2docker -> docker machine)

--- home/.oh-my-zsh-custom/docker.zsh
+++ home/.oh-my-zsh-custom/docker.zsh
@@ -1,6 +1,7 @@
 if [[ "$OSTYPE" = darwin* ]]; then
-  export DOCKER_HOST=tcp://192.168.59.103:2376
-  export DOCKER_CERT_PATH=/Users/bruno/.boot2docker/certs/boot2docker-vm
-  export DOCKER_TLS_VERIFY=1
+  export DOCKER_TLS_VERIFY="0"
+  export DOCKER_HOST="tcp://192.168.99.100:2376"
+  export DOCKER_CERT_PATH="/Users/bruno/.docker/machine/machines/default"
+  export DOCKER_MACHINE_NAME="default"
 fi
 
