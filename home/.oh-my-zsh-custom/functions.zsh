a96224c357f41a973be80ad3cdf4e1732828b6e0
Bruno Maia <bruno.mm.maia@gmail.com>
2015-10-26 18:08:27 +0000

    add utility retry function

--- home/.oh-my-zsh-custom/functions.zsh
+++ home/.oh-my-zsh-custom/functions.zsh
@@ -103,3 +103,20 @@ function vwhich() {
  vim "$(which $1)"
 }
 
+function retry {
+  local retry_max=$1
+  shift
+
+  local count=$retry_max
+  while [ $count -gt 0 ]; do
+    "$@" && break
+    count=$(($count - 1))
+    sleep 1
+  done
+
+  [ $count -eq 0 ] && {
+    echo "Retry failed [$retry_max]: $@" >&2
+    return 1
+  }
+  return 0
+}
